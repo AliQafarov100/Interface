1a9bb9b9ec428d2f301bc27f330ab0e0b64f86a6
Ali <tu8cvbxnx@code.edu.az>
2022-03-29 13:12:14 +0400

    InterFace

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MainPrintFile printedFile = new Word("Word");
+            printedFile.Print();
+
+            //PDF pdf = new PDF("Pdf");
+            //Word words = new Word("Word");
+            //MainPrintFile printFile = pdf;
+            //MainPrintFile printFile1 = words;
+            //printFile.Print();
+            //printFile1.Print();
+        }
+    }
+}
